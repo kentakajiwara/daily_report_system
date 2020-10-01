45bca347afb5814667fed482ec6e21d8775a3fe6
kenta.kajiwara <krkt.0813@rakuten.jp>
2020-10-02 08:30:40 +0900

    Add login / logout

--- src/controllers/employees/EmployeesDestroyServlet.java
+++ src/controllers/employees/EmployeesDestroyServlet.java
@@ -32,21 +32,21 @@ public class EmployeesDestroyServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId()));
-           EntityManager em = DBUtil.createEntityManager();
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
 
-           Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
-           e.setDelete_flag(1);
-           e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+            Employee e = em.find(Employee.class, (Integer)(request.getSession().getAttribute("employee_id")));
+            e.setDelete_flag(1);
+            e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
-           em.getTransaction().begin();
-           em.getTransaction().commit();
-           em.close();
-           request.getSession().setAttribute("flush","削除が完了しました。");
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+            request.getSession().setAttribute("flush", "削除が完了しました。");
 
-           response.sendRedirect(request.getContextPath() + "/employees/index");
+            response.sendRedirect(request.getContextPath() + "/employees/index");
+        }
     }
 
 }
